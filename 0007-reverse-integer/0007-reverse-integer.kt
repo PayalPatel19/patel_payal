adbafbaf166858e22cce18ecff27e4baa63cd1b4
PayalPatel19 <148843505+PayalPatel19@users.noreply.github.com>
2025-12-16 11:09:05 +0530

    Time: 115 ms (22.28%), Space: 40.5 MB (50%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun reverse(x: Int): Int {
+        var num = x
+        var reversed = 0
+
+        while(num != 0){
+            val digit = num % 10
+            num /= 10
+
+            if(reversed > Int.MAX_VALUE / 10 || (reversed == Int.MAX_VALUE / 10 && digit > 7)){
+                return 0
+            }
+
+            if(reversed < Int.MIN_VALUE / 10 || (reversed == Int.MIN_VALUE / 10 && digit < -8)){
+                return 0
+            }
+
+            reversed = reversed * 10 + digit 
+        }
+        return reversed
+    }
+}
